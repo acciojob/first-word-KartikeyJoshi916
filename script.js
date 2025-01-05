9b9d37f544525735827a80fd8db61c8b54824022
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-05 22:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
-  // your code here
-}
+	s=s.trim();
+	// if(s.length==0){
+	// 	return '';
+	// }
+	let ans="";
+	for(let i=0;i<s.length;i++){
+		if(s[i]==' '){
+			break;
+		}else{
+			ans+=s[i];
+		}
+	}
+	return ans;
+} 
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
